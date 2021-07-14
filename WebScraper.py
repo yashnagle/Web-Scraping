c5e8acb90fb363befd5c18ff7b8fc7c346bee219
yashnagle <yash.nagle1705@gmail.com>
2021-07-14 12:41:25 +0530

    updated Code

--- WebScraper.py
+++ WebScraper.py
@@ -9,7 +9,22 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
+searchQuery = input('What would you like to search?\n')
+sq = 'https://www.amazon.in/s?k='
+for w in searchQuery:
+    if w == ' ':
+        sq = sq + '+'
+    else:
+        sq = sq + w
+sq = sq + '&ref=nb_sb_noss_2'
 
+source = requests.get(sq).text
+soup = BeautifulSoup(source, 'lxml')
+count = 0
 
-
+for sp in soup.find_all('span'):
+    if count == 2:
+        break
+    print(sp)
+    count = count + 1
 
