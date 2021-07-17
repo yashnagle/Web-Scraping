80d192344375f1b40adb2305810f579bd9577afe
yashnagle <yash.nagle1705@gmail.com>
2021-07-17 19:48:07 +0530

    Completed Code

--- WebScraper.py
+++ WebScraper.py
@@ -11,10 +11,10 @@ import csv
 from os import system
 from msedge.selenium_tools import Edge, EdgeOptions
 
-
-csv_file = open('amazon_scrape.csv', 'w', encoding='utf-8')
+valDict={}
+csv_file = open('amazon_flipkart_scrape.csv', 'w', encoding='utf-8')
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['Sr. No', 'Product Name', 'Price', 'Website'])
+csv_writer.writerow(['Sr. No', 'Product Name', 'Price(Rs)', 'Link' ,'Website'])
 #Begin an instance of the webDriver
 
 url = 'https://www.amazon.in'
@@ -36,25 +36,43 @@ options.use_chromium = True
 driver = Edge(options= options)
 driver.get(url)
 
-#Extracting values
+# Extracting values
 soup = BeautifulSoup(driver.page_source, 'lxml')
-
-
+ab = 'a-size-medium a-color-base a-text-normal'
+ba = 'a-size-base-plus a-color-base a-text-normal'
 n=[]
 p=[]
+l = []
 navigation = soup.find('div', class_='a-section a-spacing-none a-padding-base').find('li',class_='a-disabled-last')
 count = 0
 while count <= 3:
 
     for result in soup.find_all('div', {'data-component-type':'s-search-result'}):
-        # print('hi')
         if result == None:
             continue
-        name = result.find('span', 'a-size-medium a-color-base a-text-normal')
+        name = result.find('span', class_=ab)
+        if name == None:
+            name = result.find('span', class_=ba)
         price = result.find('span','a-price-whole')
-        if name != None and price != None:
+        list1 = 'https://www.amazon.in/result.find'+result.find('a', class_='a-link-normal a-text-normal')['href']
+        system('cls')
+        print('name: ',name)
+        print('price: ', price)
+        print(list1)
+        if name != None:
             n.append(name.text)
+        else:
+            n.append('None')
+        
+        if price !=None:
             p.append(price.text)
+        else:
+            p.append('0')
+        if list1 != None:
+            l.append(list1)
+        else:
+            l.append('None')
+        
 
     if navigation != None:
         break
@@ -62,48 +80,90 @@ while count <= 3:
     url = 'https://www.amazon.in' + nextPage
     # print(url)
     url.format(soup.find('div', class_='a-section a-spacing-none a-padding-base').find('li',class_='a-normal').a['href'])
-    print(url)
     driver.get(url)
     soup = BeautifulSoup(driver.page_source, 'lxml')
     navigation = soup.find('div', class_='a-section a-spacing-none a-padding-base').find('li',class_='a-disabled-last')
 
+
     count = count + 1
 
 # driver.quit()
-system('cls')
+# system('cls')
 i = 0
 for i in range(len(n)):
     if i >= len(p):
         break
-    csv_writer.writerow([(i+1), n[i], '₹'+p[i], 'Amazon.in'])
+    pr = p[i].replace(',','')
+    if int(pr) in valDict:
+        valDict[int(pr)].append([0, n[i], p[i], l[i], 'Amazon.in'])
+    else:
+        valDict[int(pr)] = []
+        valDict[int(pr)].append([0, n[i], p[i], l[i], 'Amazon.in'])
 
 print('Amazon Done')
 
 url1=get_url(search,'flipkart')
 n1=[]
 p1=[]
+l1=[]
 driver.get(url1)
 soup = BeautifulSoup(driver.page_source, 'lxml')
 # s = soup.find('a', class_='_1fQZEK')
 system('cls')
 # print(s)
-driver.quit()
-for re in soup.find_all('a', class_='_1fQZEK'):
-
+# s = soup.find('div', class_='_2MImiq')
+# print(s.find('a', class_='ge-49M')['href'])
+ab = '_1fQZEK'
+ba = 's1Q9rs'
+if soup.find('a', class_=ab) == None:
+    ab = ba
+for re in soup.find_all('a', class_=ab):
     if re == None:
         continue
-    n1.append(re.find('div', class_='_4rR01T').text)
+    name = re.find('div', class_='_4rR01T')
+    if name == None:
+        name=re.find('div', class_='s1Q9rs')
+    if name == None:
+        n.append(re.text)
+    else:
+        n1.append(name.text)
     # print(re.find('div', class_='_4rR01T').text)
-    p1.append(re.find('div', class_='_30jeq3 _1_WHN1').text)
+    price = re.find('div', class_='_30jeq3 _1_WHN1')
+    if price == None:
+        price=re.find('div', class_='_30jeq3')
+    if price == None:
+        p1.append('₹0')
+    else:
+        
+        p1.append(price.text)
     # print(re.find('div', class_='_30jeq3 _1_WHN1').text)
+    l1.append('https://www.flipkart.com/'+re['href'])
+
 
 # sys.exit()
 
 for j in range(len(n1)):
     if j >= len(p1):
         break
-    csv_writer.writerow([(j+i+1), n1[j],p1[j], 'flipkart'])
+    pr = p1[j][1:].replace(',','')
 
+    if int(int(pr)) in valDict:
+        valDict[int(pr)].append([0, n1[j], p1[j][1:], l1[j],'Flipkart'])
+    else:
+        valDict[int(pr)] = []
+        valDict[int(pr)].append([0, n1[j], p1[j][1:], l1[j] ,'Flipkart'])
+
+key = list(valDict.keys())
+key.sort(reverse=True)
+
+ij = 1
+for k in key:
+    for arr in valDict[k]:
+        arr[0] = ij
+        csv_writer.writerow(arr)
+        ij = ij + 1
+csv_file.close()
+
+driver.quit()
 system('cls')
 print('DONE')
-
